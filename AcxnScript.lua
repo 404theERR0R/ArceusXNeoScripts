426bc5a9b1200f49e2b5eb2cfd0769415119d2a6
404theERR0R <joshuanamgaming@gmail.com>
2024-07-13 20:23:25 +0800

    Update AcxnScript.lua

--- AcxnScript.lua
+++ AcxnScript.lua
@@ -1,19 +1,18 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Arceus X Neo Scripts", HidePremium = false, IntroEnabled = false, IntroText = "Arceus X Neo Scripts", SaveConfig = true, ConfigFolder = "ArceusXNeoScripts"})
 
-local Tab = Window:MakeTab({
+local ScriptTab = Window:MakeTab({
 	Name = "Quick Execute Scripts",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 
---Function
-function aimbot()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/main/Scripts/ArceusX-Aimbot.lua"))();
+local Section = ScriptTab:AddSection({
+	Name = "Quick Execute Scripts"
+})
 
---Toggle
-Tab:AddButton({
+ScriptTab:AddButton({
 	Name = "Aimbot",
-	Callback = function(aimbot)
+	Callback = function()
   	end    
 })
