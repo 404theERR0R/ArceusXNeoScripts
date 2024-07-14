4c572dc4b1f09c59cc857808054f8dee2cbafb00
404theERR0R <joshuanamgaming@gmail.com>
2024-07-14 08:42:13 +0800

    Update AcxnScript.lua

--- AcxnScript.lua
+++ AcxnScript.lua
@@ -1,25 +1,49 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Arceus X Neo Scripts", HidePremium = false, SaveConfig = false, ConfigFolder = "AcxnScripts"})
 
--- Tab
+-- Tabs
 
 local Tab1 = Window:MakeTab({
-	Name = "Quick Execute",
+	Name = "Quick Hacks",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 
--- Section
+local Tab2 = Window:MakeTab({
+	Name = "Player Hacks",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+-- Sections
 
 local Section = Tab1:AddSection({
 	Name = "Scripts"
 })
 
--- Button
+local Section = Tab2:AddSection({
+	Name = "Hacks"
+})
 
+-- Buttons/Slider
+-- Tab1
 Tab1:AddButton({
 	Name = "Aimbot",
 	Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/main/Scripts/ArceusX-Aimbot.lua"))();
-		print("Script Executed")
+		print("Aimbot Executed")
+  	end    
+})
+
+Tab1:AddButton({
+	Name = "Fly",
+	Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/main/Scripts/ArceusX-Fly.lua"))();
+      		print("Fly Executed")
+  	end    
+})
+
+Tab1:AddButton({
+	Name = "Keyboard",
+	Callback = function() loadstring(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/Arceus-X-NEO-public/main/Scripts/ArceusX-Keyboard.lua"))();
+      		print("Keyboard Executed")
   	end    
 })
